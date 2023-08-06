928e0c8460625739ae0899060dccece8c55c2bd3
Paul Mick <88252430+Paul-Mick@users.noreply.github.com>
2023-08-06 17:32:32 -0600

    Update image_capture.py

--- image_capture.py
+++ image_capture.py
@@ -1,17 +1,27 @@
 import cv2
+import argparse
 
-def convert(self, video, start_num):
-    vidcap = cv2.VideoCapture(video)
+parser = argparse.ArgumentParser(description = "Capture jpeg images from .avi video")
+parser.add_argument("-n", "--number", help = "Number to start at for file naming", required = True)
+parser.add_argument("-s", "--step", help = "Step between captured frames", required = False, default = 5)
+parser.add_argument("file", help = ".avi file to capture frames from")
+parser.add_argument("directory", help = "directory to write jpeg frames to")
+args = vars(parser.parse_args())
+
+def convert():
+    vidcap = cv2.VideoCapture(args["file"])
     success, image = vidcap.read()
-    count = start_num
+    count = int(args["number"])
     frame = 0
+    step = int(args["step"])
+    directory = args["directory"]
     while success:
-        if frame % 5 == 0:
-            cv2.imwrite("./new_images/" + f"{count}" + ".jpg", image)  
+        if frame % step == 0:
+            cv2.imwrite(f"./{directory}/{count}.jpg", image)  
             print(f"Saved {count}.jpg")
             count += 1
-        success,image = vidcap.read()
+        success, image = vidcap.read()
         frame += 1
 
 if __name__ == "__main__":
-    convert("./capture.avi", 0)
+    convert()
